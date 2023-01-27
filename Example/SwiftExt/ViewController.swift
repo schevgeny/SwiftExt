081b6bbeaee1a49dc40cf38dbb5a1bb668f1cb1d
schevgeny <schevgeny@gmail.com>
2023-01-27 18:58:19 +0300

    Initial commit

---
+++ Example/SwiftExt/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SwiftExt
+//
+//  Created by schevgeny on 01/27/2023.
+//  Copyright (c) 2023 schevgeny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
